607586441065d708f6b675cb63ce106902f09b70
Akalishan <168707927+Akalishan@users.noreply.github.com>
2025-02-27 11:56:47 +0530

    feat: Add and complete navigation for Drive page

--- frontend-web/src/pages/Drive.jsx
+++ frontend-web/src/pages/Drive.jsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import img1 from "../drive_photos/img1.svg";
+import img2L from "../drive_photos/img2L.svg";
+import img2R from "../drive_photos/img2R.svg";
+import img3 from "../drive_photos/img3.svg";
+import img4 from "../drive_photos/img4.svg";
+import Navbardrive  from "../drive_components/Navbardrive";
+
 function Drive() {
-    return <div>Drive</div>;
-  }
+    return <div>
+      <div>
+        <Navbardrive />
+      </div>
+      
+    </div>;
+}
   
   export default Drive;
