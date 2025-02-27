60da3738517c0eab9297489834825babdffadbc1
Rameshshankar5 <168259788+Rameshshankar5@users.noreply.github.com>
2025-02-27 11:13:36 +0530

    feat:Add styling for Help page headings

--- frontend-web/src/pages/Help.jsx
+++ frontend-web/src/pages/Help.jsx
@@ -1,32 +1,44 @@
 import Taxi from "../assets/help_logo.svg";
+import Ride from "../assets/help_ride.svg";
+import Rent from "../assets/help_rent.svg";
+import Drive from "../assets/help_drive.svg";
 
 function Help() {
   return (
-    <div style={{
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
-      height: "450px",
-      textAlign: "center"
-    }}>
-     
-      <img src={Taxi} alt="Taxi Logo" style={{ width: "197px", height: "197px", marginBottom: "20px" }} />
-
+    <div className="flex flex-col items-center justify-center h-[750px] text-center">
       
-      <h2 style={{
-        fontSize: "48px",
-        fontFamily: "Inter",
-        fontWeight: "400",
-        wordWrap: "break-word",
-        background: "linear-gradient(90deg, #FFD12E, #FF7C1D)",
-        WebkitBackgroundClip: "text",  
-        WebkitTextFillColor: "transparent" 
-      }}>
+      <img src={Taxi} alt="Taxi Logo" className="w-[197px] h-[197px] mb-5" />
+
+      <h2 className="text-4xl font-light bg-gradient-to-r from-yellow-400 to-orange-500 bg-clip-text text-transparent">
         Welcome to Urban Drive Support
       </h2>
+
+      {/* Image Row */}
+      <div className="flex flex-wrap justify-center gap-75 mt-8">
+        
+        {/* Ride Box */}
+        <div className="w-[220px] p-4 border-4 border-yellow-400 rounded-lg flex flex-col items-center">
+          <img src={Ride} alt="Ride" className="w-[180px] h-[180px]" />
+          <div className="text-4xl font-light bg-gradient-to-r from-yellow-400 to-orange-500 bg-clip-text text-transparent">RIDE</div>
+        </div>
+
+        {/* Rent Box */}
+        <div className="w-[220px] p-4 border-4 border-yellow-400 rounded-lg flex flex-col items-center">
+          <img src={Rent} alt="Rent" className="w-[180px] h-[180px]" />
+          <div className="text-4xl font-light bg-gradient-to-r from-yellow-400 to-orange-500 bg-clip-text text-transparent">RENT</div>
+        </div>
+
+        {/* Drive Box */}
+        <div className="w-[220px] p-4 border-4 border-yellow-400 rounded-lg flex flex-col items-center">
+          <img src={Drive} alt="Drive" className="w-[180px] h-[180px]" />
+          <div className="text-4xl font-light bg-gradient-to-r from-yellow-400 to-orange-500 bg-clip-text text-transparent">DRIVE</div>
+        </div>
+
+      </div>
+      
     </div>
   );
 }
 
 export default Help;
+
