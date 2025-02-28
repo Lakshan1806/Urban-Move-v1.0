bac8d0cf95f6ed10baf9dbbb00509cd8f2c442f2
Akalishan <168707927+Akalishan@users.noreply.github.com>
2025-02-28 08:58:37 +0530

    feat:Add left section of the second page of Drive

--- frontend-web/src/pages/Drive.jsx
+++ frontend-web/src/pages/Drive.jsx
@@ -1,13 +1,13 @@
 /*import { useState } from "react";
 import { Link } from "react-router-dom";
-
-import img2L from "../drive_photos/img2L.svg";
-import img2R from "../drive_photos/img2R.svg";
 import img3 from "../drive_photos/img3.svg";
 import img4 from "../drive_photos/img4.svg";*/
 import Navbardrive from "../drive_components/Navbardrive";
 import Drive1stpageleft from "../drive_components/drive1stpageleft";
 import img1 from "../drive_photos/img1.svg";
+
+import img2R from "../drive_photos/img2R.svg";
+import Drive2ndpageleft from "../drive_components/drive2ndpageleft"
 
 function Drive() {
   return (
@@ -20,6 +20,21 @@ function Drive() {
         </div>
         <div>
           <img src={img1} alt="img1" className="w-[1500px] h-[600px]" />
+        </div>
+
+      </div>
+      <div>
+        <div className="p-[10px] text-center text-[36px] font-[700] font-sans">
+          Why become a rideshare driver?
+        </div>
+        <div>
+          <div>
+            <Drive2ndpageleft />
+          </div>
+          <div>
+
+          </div>
+
         </div>
 
       </div>
