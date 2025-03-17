8d6cde29f631a785c4e3ebd848ab1212de7aff29
Lakshan1806 <lakshanuthayan@gmail.com>
2025-03-17 16:15:12 +0530

    feat: Add Backend Config Change

--- backend/server.js
+++ backend/server.js
@@ -5,19 +5,13 @@ import cors from "cors";
 import adminRoutes from "./routes/adminRoute.js";
 import checkAndCreateAdmin from "./utils/adminInitialSetup.js";
 import feedbackRoutes from "./routes/feedbackRoute.js";
-import cookieParser from "cookie-parser";import feedbackRoutes from "./routes/feedbackRoute.js";
+import cookieParser from "cookie-parser";
+import feedbackRoutes from "./routes/feedbackRoute.js";
 
 dotenv.config();
 const app = express();
-app.use(cors());
-
-
-
-app.use(cors());
-
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
@@ -28,9 +22,6 @@ app.use(
 
 app.use(cookieParser());
 app.use("/admin", adminRoutes);
-app.use("/feedback", feedbackRoutes);
-
-app.use(express.urlencoded({ extended: false }));
 app.use("/feedback", feedbackRoutes);
 
 console.log(process.env.MONGO_URI);
