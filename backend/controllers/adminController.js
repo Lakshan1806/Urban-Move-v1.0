6976e190f6fe6152839496f7e36d7c20c29717be
Lakshan1806 <lakshanuthayan@gmail.com>
2025-04-16 15:17:08 +0530

    chore: Add Config Change

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,9 +1,9 @@
-import authController from "./authController.js";
+import adminAuthController from "./adminAuthController.js";
 import adminAccountManagementController from "./adminAccountManagementController.js";
 import carManagementController from "./carManagementController.js";
 
 const adminController = {
-  auth: authController,
+  auth: adminAuthController,
   account: adminAccountManagementController,
   car: carManagementController,
 };
