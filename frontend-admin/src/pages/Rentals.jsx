37e993490899a352e67d1635d4f8b00d9759a85d
Lakshan1806 <lakshanuthayan@gmail.com>
2025-05-20 05:40:45 +0530

    refactor: Revert to full-height(of the parent)  Grid sections
    Replaced Flexbox-based tile containers with multiple full-height grid sections. Each section uses Tailwind’s grid-cols, grid-rows and h-full for automatic tile placement, allowing users to scroll between grids.

--- frontend-admin/src/pages/Rentals.jsx
+++ frontend-admin/src/pages/Rentals.jsx
@@ -19,31 +19,24 @@ function Rentals() {
 
   return (
     <div className="h-full flex flex-col">
-      <div className="flex  justify-between flex-none">
+      <div className="flex flex-row justify-between flex-none">
         <h1 className="[-webkit-text-stroke:1px_rgb(255,124,29)] font-[700] text-[36px]">
           Rentals
         </h1>
       </div>
-      <div className="flex-1 min-h-0 flex flex-col gap-3 overflow-y-auto">
-        <div className="flex h-full flex-none gap-3">
-          <div className="w-1/3  p-4 rounded shadow-[0px_10px_20px_0px_rgba(0,_0,_0,_0.15)] overflow-auto shrink-0">
-            {component}
-          </div>
-          <div className="flex-1 shrink-0   p-4 rounded shadow-[0px_10px_20px_0px_rgba(0,_0,_0,_0.15)] overflow-auto">
+      <div className="flex-1 flex flex-col gap-3 overflow-y-auto">
+        <div className="grid grid-cols-12 grid-rows-12 gap-3  h-full">
+          {component}
+
+          <div className="col-span-8 row-span-12 p-4 rounded shadow-[0px_10px_20px_0px_rgba(0,_0,_0,_0.15)] overflow-auto">
             <CarDetails car={carDetails} onUpdate={setCarDetails} />
           </div>
         </div>
-        <div className="flex h-full flex-none gap-3">
-          <div className="w-2/3 shrink-0   p-4 rounded shadow-[0px_10px_20px_0px_rgba(0,_0,_0,_0.15)] overflow-auto">
-            <div>Rental History</div>
-          </div>
-          <div className="flex flex-col h-full flex-1  gap-3 shadow-[0px_10px_20px_0px_rgba(0,_0,_0,_0.15)]">
-            <div className=" h-1/2 p-4 rounded shadow-[0px_10px_20px_0px_rgba(0,_0,_0,_0.15)] overflow-auto shrink-0">
-              <div>Recently Deleted Cars</div>
-            </div>
-            <div className=" h-1/2 p-4 rounded shadow-[0px_10px_20px_0px_rgba(0,_0,_0,_0.15)] overflow-auto shrink-0">
-              <div>Recently Deleted Units</div>
-            </div>
+        <div className="grid grid-cols-12 grid-rows-12 gap-3 h-full">
+          {component}
+
+          <div className="col-span-8 row-span-12 p-4 rounded shadow-[0px_10px_20px_0px_rgba(0,_0,_0,_0.15)] overflow-auto">
+            <CarDetails car={carDetails} onUpdate={setCarDetails} />
           </div>
         </div>
       </div>
