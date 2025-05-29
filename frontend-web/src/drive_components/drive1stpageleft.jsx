5918aeb8fdea287bbf799d6014da4a77f4005fbc
Akalishan <168707927+Akalishan@users.noreply.github.com>
2025-05-30 01:50:28 +0530

    feat: Enhance layout and styling of Drive pages with responsive design adjustments

--- frontend-web/src/drive_components/drive1stpageleft.jsx
+++ frontend-web/src/drive_components/drive1stpageleft.jsx
@@ -1,22 +1,30 @@
-import { Link } from "react-router-dom"; 
-       
- function Drive1stpageleft(){
+import { Link } from "react-router-dom";
+import img1 from "../drive_photos/img1.svg";
 
-    return(
+function Drive1stpageleft() {
+  return (
+    <div className="justify-center items-center col-span-12 row-span-12">
+      <div className="flex h-full w-full min-h-0">
+        <div className="flex flex-col w-1/3 min-h-0 h-full">
+          <p>
+            Drive on your schedule <br />
+            and earn what you <br />
+            deserve.
+          </p>
 
-    <div className="flex-col w-[522px] px-[0px] py-[200px] justify-center items-center gap-[10px] self-stretch font-sans grid justify-items-center ">
-        
-            <p className= "w-[522px] text-black  text-[44px] not-italic font-[400] leading-12 stroke-[1px] stroke-black mx-28 font-sans ">
-                Drive on your schedule 
-                and earn what you 
-                deserve.</p>
-        
-           <p> <Link className="text-black p-4 px-0.5 gap-[10px] text-[20px] font-[700] items-center leading-24 font-sans">Already have an account? Sign in</Link></p>
-        
-            <button className="w-[135px] h-[44px] bg-gradient-to-r from-[#FFD12E] to-[#FF7C1D] bg-clip-text text-transparent cursor-pointer rounded-[50px] border border-[#FFD12E] font-sans ">Get Started </button>
-        
+          <Link
+            to="/dLogin"
+            className="text-black p-4 text-[20px] font-[700] leading-6"
+          >
+            Already have an account? Sign in
+          </Link>
+        </div>
+        <div className="w-2/3 h-full">
+         {/*  <img src={img1} alt="img1" className="h-full w-auto max-w-full" /> */}
+        </div>
+      </div>
     </div>
+  );
+}
 
-    );
- }
- export default Drive1stpageleft;
+export default Drive1stpageleft;
