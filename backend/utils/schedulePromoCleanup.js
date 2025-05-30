31f5605dc4c4cd61e223cefb868ad66453465c5d
Lakshan1806 <lakshanuthayan@gmail.com>
2025-05-30 14:19:38 +0530

    chore: Add temporary logic for revert car unit state to available

--- backend/utils/schedulePromoCleanup.js
+++ backend/utils/schedulePromoCleanup.js
@@ -1,6 +1,7 @@
 import Promotion from "../models/promotion.model.js";
+import CarInstance from "../models/carInstance.model.js"; //temporary import
 import cron from "node-cron";
-
+ 
 function schedulePromoCleanup() {
   async function deactivateExpiredPromoCodes() {
     try {
@@ -10,7 +11,18 @@ function schedulePromoCleanup() {
       );
       console.log(`Promo cleanup: ${update.modifiedCount} promo codes deactivated`);
     } catch (err) {
-      console.error("Error deactivating promos:", err);
+      console.error("Error deactivating promos:", err); 
+    }
+
+    // temporary.............
+    try {
+      const update = await CarInstance.updateMany( 
+        { status: "Booked" }, 
+        { $set: { status: "Available" } }
+      );
+      console.log(`instance cleanup: ${update.modifiedCount} instances revoked`);
+    } catch (err) { 
+      console.error("Error:", err);
     }
   }
 
