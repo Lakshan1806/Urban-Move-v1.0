8681da5c7466b73f9413ceeee7f0f12edb51534f
Akalishan <168707927+Akalishan@users.noreply.github.com>
2025-05-30 15:53:43 +0530

    chore: update dependencies and restructure project

--- frontend-driver/src/components/DriverRouteSelect.jsx
+++ frontend-driver/src/components/DriverRouteSelect.jsx
@@ -1,20 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import DriverProtectedRoute from "./DriverProtectedRoute";
+//import DriverProtectedRoute from "./DriverProtectedRoute";
 import DriverHome from "../pages/driverHome";
 function DriverRouteSelect() {
   return (
     <div>
       <main className="h-dvh ">
         <Routes>
-          
-          <Route
-            path="/"
-            element={
-              <DriverProtectedRoute>
-                <DriverHome />
-              </DriverProtectedRoute>
-            }
-          />
+          <Route path="/" element={<DriverHome />} />
         </Routes>
       </main>
     </div>
--- frontend-driver/src/components/Navbar.jsx
+++ frontend-driver/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { DriverAuthContext } from "../context/DriverAuthContext";
+import { DriverAuthContext } from "../context/DriverAuthContext-driver";
 import Logo from "../assets/Urban_Move_Colour.svg";
 import { FaUser, FaBell, FaSignOutAlt } from "react-icons/fa";
 
@@ -9,13 +9,13 @@ function NavBar() {
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const navigate = useNavigate();
 
-  useEffect(() => {
+  /* useEffect(() => {
     console.log(
       "Navbar detected authentication change:",
       isAuthenticated,
       driver
     );
-  }, [isAuthenticated, driver]);
+  }, [isAuthenticated, driver]); */
 
   const navItems = [
     { path: "/driver/home", label: "Home" },
@@ -23,7 +23,6 @@ function NavBar() {
     { path: "/banking", label: "Banking" },
     { path: "/help", label: "Help" },
   ];
-
 
   const accountOptions = [
     { path: "/profile", label: "My Profile", icon: <FaUser /> },
@@ -43,9 +42,8 @@ function NavBar() {
   const linkstyles1 =
     "font-sans bg-gradient-to-r from-[#FFD12E] to-[#FF7C1D] text-transparent bg-clip-text text-[15px] uppercase";
 
-  
   return (
-    <nav className="bg-black w-full h-[80px] flex flex-row items-center py-[10px] px-[40px] justify-between">
+    <nav className="bg-black w-full h-[80px] flex flex-row items-center  px-[40px] justify-between top-0 sticky">
       <header>
         <img src={Logo} alt="Logo" className="w-[60px] h-[60px]" />
       </header>
@@ -57,51 +55,53 @@ function NavBar() {
         ))}
       </div>
       <div className="flex gap-[10px]">
-        <button
-          onClick={() => setDropdownOpen(!dropdownOpen)}
-          className="flex items-center space-x-2 px-4 py-2 border rounded-full text-orange-400 border-orange-400 hover:bg-orange-400 hover:text-black transition"
-        >
-          <FaUser />
-          <span>ACCOUNT</span>
-          <span>{dropdownOpen ? "▲" : "▼"}</span>
-        </button>
+        <div className="relative">
+          <button
+            onClick={() => setDropdownOpen(!dropdownOpen)}
+            className="flex items-center space-x-2 px-4 py-2 border rounded-full text-orange-400 border-orange-400 hover:bg-orange-400 hover:text-black transition"
+          >
+            <FaUser />
+            <span>ACCOUNT</span>
+            <span>{dropdownOpen ? "▲" : "▼"}</span>
+          </button>
 
-        {dropdownOpen && (
-          <>
-            <div
-              className="fixed inset-0 z-40"
-              onClick={() => setDropdownOpen(false)}
-            />
+          {dropdownOpen && (
+            <>
+              <div
+                className="fixed inset-0 z-40"
+                onClick={() => setDropdownOpen(false)}
+              />
 
-            <div className="absolute right-0 mt-2 bg-black text-orange-400 border border-orange-400 rounded-xl p-3 space-y-2 shadow-lg z-50">
-              {accountOptions.map((option) =>
-                option.path ? (
-                  <Link
-                    to={option.path}
-                    key={option.path || option.label}
-                    className="flex items-center gap-2 p-2 hover:bg-gray-800 cursor-pointer rounded"
-                    onClick={() => setDropdownOpen(false)}
-                  >
-                    {option.icon}
-                    <span>{option.label}</span>
-                  </Link>
-                ) : (
-                  <div
-                    key={option.label}
-                    onClick={() => {
-                      option.action();
-                      setDropdownOpen(false);
-                    }}
-                    className="flex items-center gap-2 p-2 hover:bg-gray-800 cursor-pointer rounded"
-                  >
-                    {option.icon}
-                    <span>{option.label}</span>
-                  </div>
-                )
-              )}
-            </div>
-          </>
-        )}
+              <div className="absolute right-0 mt-2 bg-black text-orange-400 border border-orange-400 rounded-xl p-3 space-y-2 shadow-lg z-50">
+                {accountOptions.map((option) =>
+                  option.path ? (
+                    <Link
+                      to={option.path}
+                      key={option.path || option.label}
+                      className="flex items-center gap-2 p-2 hover:bg-gray-800 cursor-pointer rounded"
+                      onClick={() => setDropdownOpen(false)}
+                    >
+                      {option.icon}
+                      <span>{option.label}</span>
+                    </Link>
+                  ) : (
+                    <div
+                      key={option.label}
+                      onClick={() => {
+                        option.action();
+                        setDropdownOpen(false);
+                      }}
+                      className="flex items-center gap-2 p-2 hover:bg-gray-800 cursor-pointer rounded"
+                    >
+                      {option.icon}
+                      <span>{option.label}</span>
+                    </div>
+                  )
+                )}
+              </div>
+            </>
+          )}
+        </div>
       </div>
     </nav>
   );
