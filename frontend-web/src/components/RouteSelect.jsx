f0a363f86a34dbfce86b8a5eccd050d7a4bb8bdc
Lakshan1806 <lakshanuthayan@gmail.com>
2025-05-30 17:29:51 +0530

    fix: Update RouteSelect.jsx

--- frontend-web/src/components/RouteSelect.jsx
+++ frontend-web/src/components/RouteSelect.jsx
@@ -24,47 +24,7 @@ function RouteSelect() {
   const userId = "123456"; // Replace this with actual logged-in user ID
 
   return (
-<<<<<<< Updated upstream
-    <div>
-      <main className="h-full shrink-0 ">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/rent" element={<Rent />} />
-          <Route path="/drive" element={<Drive />} />
-          <Route path="/ride" element={<Ride />} />
-          <Route path="/help" element={<Help />} />
-          <Route path="/payment" element={<PaymentPage />} />
-          <Route path="/signup" element={<Signupinitialpage />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/dRegister" element={<DriverRegister />} />
-          <Route path="/dLogin" element={<DriverLogin />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signin" element={<Signininitialpage />} />
-          <Route path="/email" element={<EmailForm />} />
-          <Route path="/feedback" element={<Feedback />} />
-          <Route
-            path="/trip-history"
-            element={<TripHistory userId={userId} />}
-          />
-          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-          <Route
-            path="/reset-password/:token"
-            element={<ResetpasswordPage />}
-          />
-          <Route
-            path="/profile"
-            element={
-              <ProtectedRoute>
-                <Profile />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="/verify-phone" element={<GooglePhoneVerification />} />
-        </Routes>
-      </main>
-    </div>
-=======
-    <main className="h-full min-h-0 overflow-hidden">
+    <main className="h-full min-h-0 overflow-auto">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/rent" element={<Rent />} />
@@ -94,7 +54,6 @@ function RouteSelect() {
         <Route path="/verify-phone" element={<GooglePhoneVerification />} />
       </Routes>
     </main>
->>>>>>> Stashed changes
   );
 }
 
