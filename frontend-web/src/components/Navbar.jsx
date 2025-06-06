795c7bcea9a43f7e61b38bfa2c9633c55f65da1d
Akalishan <168707927+Akalishan@users.noreply.github.com>
2025-06-06 08:15:39 +0530

    feat: Refactor Navbar and Drive components for improved responsiveness and UI enhancements

--- frontend-web/src/components/Navbar.jsx
+++ frontend-web/src/components/Navbar.jsx
@@ -2,11 +2,13 @@ import React, { useState, useContext, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 import Logo from "../assets/Urban_Move_Colour.svg";
-import { FaUser, FaBell, FaSignOutAlt } from "react-icons/fa";
+import { FaUser, FaBell, FaSignOutAlt, FaBars } from "react-icons/fa";
 
 function NavBar() {
   const { isAuthenticated, logout, user } = useContext(AuthContext);
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [mobileOpen, setMobileOpen] = useState(false);
+
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -52,75 +54,169 @@ function NavBar() {
     "font-sans bg-gradient-to-r from-[#FFD12E] to-[#FF7C1D] text-transparent bg-clip-text text-[15px] uppercase rounded-[50px]";
 
   return (
-    <nav className="bg-black w-full  z-20 h-full px-[25px] flex flex-row items-center justify-between top-0 sticky">
-      <header>
-        <img src={Logo} alt="Logo" className="w-[60px] h-[60px]" />
-      </header>
-      <div className="flex gap-[40px]">
-        {navItems.map((item) => (
-          <Link to={item.path} key={item.path} className={linkstyles1}>
-            {item.label}
-          </Link>
-        ))}
-      </div>
-      <div className="flex gap-[10px]">
-        {!isAuthenticated ? (
-          accAuth.map((item) => (
-            <Link to={item.path} key={item.path} className={linkstyles2}>
+    <>
+      <nav className=" bg-black w-full  z-20 h-full px-[25px] flex flex-row items-center justify-between top-0 sticky">
+        <div className="flex justify-between items-center w-full lg:w-auto">
+          <div className="relative flex-1 flex justify-center lg:justify-start ">
+            <img src={Logo} alt="Logo" className="w-[60px] h-[60px] " />
+          </div>
+          <div className="absolute right-0 lg:hidden">
+            <button
+              onClick={() => setMobileOpen(!mobileOpen)}
+              className="text-orange-400 text-2xl"
+            >
+              <FaBars />
+            </button>
+          </div>
+        </div>
+        <div className="hidden lg:flex gap-8 items-center ">
+          {navItems.map((item) => (
+            <Link to={item.path} key={item.path} className={linkstyles1}>
               {item.label}
             </Link>
-          ))
-        ) : (
-          <div className="relative">
-            <button
-              onClick={() => setDropdownOpen(!dropdownOpen)}
-              className="flex items-center space-x-2 px-4 py-2 border rounded-full text-orange-400 border-orange-400 hover:bg-orange-400 hover:text-black transition"
-            >
-              <FaUser />
-              <span>ACCOUNT</span>
-              <span>{dropdownOpen ? "▲" : "▼"}</span>
-            </button>
+          ))}
+        </div>
+        <div className="hidden lg:flex gap-[10px]">
+          {!isAuthenticated ? (
+            accAuth.map((item) => (
+              <Link to={item.path} key={item.path} className={linkstyles2}>
+                {item.label}
+              </Link>
+            ))
+          ) : (
+            <div className="relative">
+              <button
+                onClick={() => setDropdownOpen(!dropdownOpen)}
+                className="flex items-center space-x-2 px-4 py-2 border rounded-full text-orange-400 border-orange-400 hover:bg-orange-400 hover:text-black transition"
+              >
+                <FaUser />
+                <span>ACCOUNT</span>
+                <span>{dropdownOpen ? "▲" : "▼"}</span>
+              </button>
 
-            {dropdownOpen && (
-              <>
-                <div
-                  className="fixed inset-0 z-40"
-                  onClick={() => setDropdownOpen(false)}
-                />
+              {dropdownOpen && (
+                <>
+                  <div
+                    className="fixed inset-0 z-40"
+                    onClick={() => setDropdownOpen(false)}
+                  />
 
-                <div className="absolute right-0 mt-2 bg-black text-orange-400 border border-orange-400 rounded-xl p-3 space-y-2 shadow-lg z-50">
-                  {accountOptions.map((option) =>
-                    option.path ? (
-                      <Link
-                        to={option.path}
-                        key={option.path || option.label}
-                        className="flex items-center gap-2 p-2 hover:bg-gray-800 cursor-pointer rounded"
-                        onClick={() => setDropdownOpen(false)}
-                      >
-                        {option.icon}
-                        <span>{option.label}</span>
-                      </Link>
-                    ) : (
-                      <div
-                        key={option.label}
-                        onClick={() => {
-                          option.action();
-                          setDropdownOpen(false);
-                        }}
-                        className="flex items-center gap-2 p-2 hover:bg-gray-800 cursor-pointer rounded"
-                      >
-                        {option.icon}
-                        <span>{option.label}</span>
-                      </div>
-                    )
-                  )}
-                </div>
-              </>
-            )}
-          </div>
-        )}
-      </div>
-    </nav>
+                  <div className="absolute right-0 mt-2 bg-black text-orange-400 border border-orange-400 rounded-xl p-3 space-y-2 shadow-lg z-50">
+                    {accountOptions.map((option) =>
+                      option.path ? (
+                        <Link
+                          to={option.path}
+                          key={option.path || option.label}
+                          className="flex items-center gap-2 p-2 hover:bg-gray-800 cursor-pointer rounded"
+                          onClick={() => setDropdownOpen(false)}
+                        >
+                          {option.icon}
+                          <span>{option.label}</span>
+                        </Link>
+                      ) : (
+                        <div
+                          key={option.label}
+                          onClick={() => {
+                            option.action();
+                            setDropdownOpen(false);
+                          }}
+                          className="flex items-center gap-2 p-2 hover:bg-gray-800 cursor-pointer rounded"
+                        >
+                          {option.icon}
+                          <span>{option.label}</span>
+                        </div>
+                      )
+                    )}
+                  </div>
+                </>
+              )}
+            </div>
+          )}
+        </div>
+      </nav>
+      {mobileOpen && (
+        <div className="lg:hidden flex flex-col bg-black text-orange-400 px-5 py-3 gap-4 z-30 relative justify-center">
+          {navItems.map((item) => (
+            <div key={item.path} className="flex justify-center">
+              <Link
+                to={item.path}
+                key={item.path}
+                className="text-lg border-b border-orange-400 pb-2"
+                onClick={() => setMobileOpen(false)}
+              >
+                {item.label}
+              </Link>
+            </div>
+          ))}
+
+          {!isAuthenticated ? (
+            accAuth.map((item) => (
+              <div key={item.path} className="flex justify-center">
+                <Link
+                  to={item.path}
+                  key={item.path}
+                  className="text-lg border-b border-orange-400 pb-2"
+                  onClick={() => setMobileOpen(false)}
+                >
+                  {item.label}
+                </Link>
+              </div>
+            ))
+          ) : (
+            <div className="relative">
+              <button
+                onClick={() => setDropdownOpen(!dropdownOpen)}
+                className="flex items-center space-x-2 px-4 py-2 border rounded-full text-orange-400 border-orange-400 hover:bg-orange-400 hover:text-black transition w-full justify-center"
+              >
+                <FaUser />
+                <span>ACCOUNT</span>
+                <span>{dropdownOpen ? "▲" : "▼"}</span>
+              </button>
+
+              {dropdownOpen && (
+                <>
+                  <div
+                    className="fixed inset-0 z-40"
+                    onClick={() => setDropdownOpen(false)}
+                  />
+                  <div className=" mt-2 bg-black text-orange-400 border border-orange-400 rounded-xl p-3 space-y-2 shadow-lg z-50 relative">
+                    {accountOptions.map((option) =>
+                      option.path ? (
+                        <Link
+                          to={option.path}
+                          key={option.path}
+                          className="text-lg border-b border-orange-400 pb-2 flex items-center gap-2"
+                          onClick={() => {
+                            setMobileOpen(false);
+                            setDropdownOpen(false);
+                          }}
+                        >
+                          {option.icon}
+                          {option.label}
+                        </Link>
+                      ) : (
+                        <div
+                          key={option.label}
+                          className="text-lg border-b border-orange-400 pb-2 flex items-center gap-2 cursor-pointer"
+                          onClick={() => {
+                            option.action();
+                            setMobileOpen(false);
+                            setDropdownOpen(false);
+                          }}
+                        >
+                          {option.icon}
+                          {option.label}
+                        </div>
+                      )
+                    )}
+                  </div>
+                </>
+              )}
+            </div>
+          )}
+        </div>
+      )}
+    </>
   );
 }
 
--- frontend-web/src/pages/Drive.jsx
+++ frontend-web/src/pages/Drive.jsx
@@ -11,27 +11,43 @@ import Footer from "../components/Footer";
 function Drive() {
   return (
     <div className="h-full flex flex-col ">
-      <div className="flex flex-row">
+      <div className="flex ">
         <Navbardrive />
       </div>
       <div className="flex-1 flex flex-col overflow-y-auto  min-h-0 snap-y snap-mandatory scroll-smooth">
         <div className="grid grid-cols-12 grid-rows-12 h-full shrink-0 snap-start">
           <Drive1stpageleft />
         </div>
-        <div className="grid grid-cols-12 grid-rows-12 h-full shrink-0 snap-start">
-          <div className="col-span-12 row-span-1 p-[10px] text-center text-[30px] font-semibold flex justify-center items-center">
+        <div className="hidden lg:grid grid-cols-12 grid-rows-12 h-full shrink-0 snap-start">
+          <div className="col-span-12 px-4 py-2 text-center font-semibold flex justify-center items-center text-xl sm:text-2xl md:text-3xl lg:text-[30px]">
             Why become a rideshare driver?
           </div>
-
-          <div className="col-span-6 row-start-2 row-span-11">
+          <div className="col-span-12 lg:col-span-6 h-dvh">
             <Drive2ndpageleft />
           </div>
-
-          <div className="col-span-6 row-start-2 row-span-11">
+          <div className="col-span-12 lg:col-span-6 h-dvh">
             <Drive2ndpageright />
           </div>
         </div>
-        <div className="grid grid-cols-12 grid-rows-12 h-full shrink-0 snap-start">
+
+        {/* Second page - Mobile version (below lg) - Left section */}
+        <div className="lg:hidden grid grid-cols-12 grid-rows-12 h-full shrink-0 snap-start">
+          <div className="col-span-12 px-4 py-2 text-center font-semibold flex justify-center items-center text-xl sm:text-2xl md:text-3xl">
+            Why become a rideshare driver?
+          </div>
+          <div className="col-span-12 h-full">
+            <Drive2ndpageleft />
+          </div>
+        </div>
+
+        {/* Second page - Mobile version (below lg) - Right section */}
+        <div className="lg:hidden grid grid-cols-12 grid-rows-12 h-full shrink-0 snap-start">
+          <div className="col-span-12 h-full">
+            <Drive2ndpageright />
+          </div>
+        </div>
+
+        <div className="hidden lg:grid grid-cols-12 grid-rows-12 h-screen lg:h-full shrink-0 snap-start">
           <h1 className=" col-span-12 row-span-1 text-3xl font-semibold text-center">
             Here's what you need to become a driver
           </h1>
@@ -46,7 +62,28 @@ function Drive() {
             <Drive3rdpage />
           </div>
         </div>
-        <div className="grid grid-cols-12 grid-rows-12 h-full shrink-0 snap-start">
+        {/* 3 rd page - Mobile version (below lg) - heading & image */}
+        <div className="lg:hidden flex flex-col h-full shrink-0 snap-start">
+          <h1 className="text-2xl md:text-3xl font-semibold text-center px-4 py-4 bg-white">
+            Here's what you need to become a driver
+          </h1>
+          <div className="flex-1">
+            <img
+              src={img3}
+              alt="img3"
+              className=" w-full h-full object-center"
+            />
+          </div>
+        </div>
+
+        {/* Third page - Mobile version - Content section */}
+        <div className="lg:hidden grid grid-cols-12 grid-rows-12 h-full shrink-0 snap-start">
+          <div className="col-span-12 row-span-12 w-full h-full ">
+            <Drive3rdpage />
+          </div>
+        </div>
+
+        <div className="hidden lg:grid grid-cols-12 grid-rows-12 h-screen lg:h-full shrink-0 snap-start">
           <h1 className=" col-span-12 row-span-1 text-3xl font-semibold text-center">
             Safty on the road
           </h1>
@@ -58,6 +95,26 @@ function Drive() {
           />
 
           <div className="col-span-12 row-span-5 w-full h-full ">
+            <Drive4thpage />
+          </div>
+        </div>
+
+        {/* 4th page - Mobile version (below lg) - heading & image */}
+        <div className="lg:hidden flex flex-col h-full shrink-0 snap-start">
+          <h1 className="text-2xl md:text-3xl font-semibold text-center px-4 py-4 bg-white">
+            Safty on the road
+          </h1>
+
+          <img
+            src={img4}
+            alt="img4"
+            className="col-span-12  row-span-7 w-full h-full object-cover"
+          />
+        </div>
+
+        {/* 4th page - Mobile version - Content section */}
+        <div className="lg:hidden grid grid-cols-12 grid-rows-12 h-full shrink-0 snap-start">
+          <div className="col-span-12 row-span-12 w-full h-full ">
             <Drive4thpage />
           </div>
         </div>
