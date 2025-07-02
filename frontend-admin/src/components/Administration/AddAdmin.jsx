87f74d1955828f694f3cd83ff2718449919c0c65
Lakshan1806 <lakshanuthayan@gmail.com>
2025-07-02 09:22:56 +0530

    feat: Refactor AddAdmin to use icon component and update styles
    Replaced the Line.svg image with the TfiLayoutLineSolid icon from react-icons for improved scalability and consistency. Updated layout and spacing for the AddAdmin component, including form padding and button alignment, and applied a gradient stroke to the heading.

--- frontend-admin/src/components/Administration/AddAdmin.jsx
+++ frontend-admin/src/components/Administration/AddAdmin.jsx
@@ -1,5 +1,5 @@
 import Logo from "../../assets/Urban_Move.svg";
-import Line from "../../assets/Line.svg";
+import { TfiLayoutLineSolid } from "react-icons/tfi";
 import { useState } from "react";
 import axios from "axios";
 import { GoPlusCircle } from "react-icons/go";
@@ -23,18 +23,25 @@ function AddAdmin() {
   };
 
   return (
-    <div className="col-span-4 row-span-12 p-4 rounded shadow-[0px_10px_20px_0px_rgba(0,_0,_0,_0.15)] flex flex-col h-full justify-center items-stretch gap-10">
-      <div className="flex flex-col items-center [-webkit-text-stroke:1px_rgb(255,124,29)] font-[700] text-[36px]">
-        <h1>Add admin account</h1>
+    <div className="col-span-4 row-span-12 p-4 rounded shadow-[0px_10px_20px_0px_rgba(0,_0,_0,_0.15)] flex flex-col h-full justify-center gap-5">
+      <div className="flex flex-row  justify-center flex-none">
+        <h1
+          className="text-grad-stroke font-[700] text-[36px]"
+          data-text="Add admin account"
+        >
+          Add admin account
+        </h1>
       </div>
-      <div className="flex flex-row justify-center">
+      <div className="flex flex-row justify-center pb-8">
         <img src={Logo} className="w-[200px] h-[200px]" />
       </div>
 
-      <img src={Line} />
-
+      <TfiLayoutLineSolid
+        className="h-12 w-full block flex-none [&>path:not([fill='none'])]:fill-[url(#icon-gradient)]"
+        preserveAspectRatio="none"
+      />
       <div>
-        <form onSubmit={addAdmin} className="flex flex-col gap-10 p-8">
+        <form onSubmit={addAdmin} className="flex flex-col gap-10 pb-8 px-8">
           <div className="flex flex-col">
             <label htmlFor="username">Username</label>
             <input
@@ -91,7 +98,7 @@ function AddAdmin() {
           <div className="button-wrapper">
             <button
               type="submit"
-              className="button-primary flex flex-row items-center gap-2"
+              className="button-primary flex flex-row items-center justify-center gap-2"
             >
               <GoPlusCircle className="[&>path]:fill-[url(#icon-gradient)]" />
               ADD ADMIN
